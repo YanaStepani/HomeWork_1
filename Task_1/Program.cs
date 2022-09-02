a3d743f693668515811aa45ec90d36598f947371
Yana <yana.stepani@gmail.com>
2022-09-02 13:36:35 +0700

    написание программы решения задачи 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+// получение данных от пользователя
+Console.Write("Введите число 1: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+// присвоение переменных max и min
+int max = number_1;
+int min = number_2;
+
+// поиск максимального и минимального значения
+if(max < number_2)
+{
+    max = number_2;
+    min = number_1;
+}
+
+// вывод ответа пользователю
+Console.WriteLine($"Максимальное число = {max}, минимальное число = {min}.);
