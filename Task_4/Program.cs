0ad015975f7309c0ac2cca4598819e370f32a05f
Yana <yana.stepani@gmail.com>
2022-09-02 16:50:31 +0700

    Запись ешения задачи 4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// напишите программу, которая на входе принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+// получение числа от пользвателя
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+// введение переменых
+int index = 0;
+int result = 0;
+
+//проверка условия (четности числа)
+while(index < number)
+{
+    if(number%2 == 0)
+    {
+        result = number;
+        Console.WriteLine(result);
+    }
+    index++; 
+}
